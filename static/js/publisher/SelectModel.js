db3dd4b674edecfde070dcf196cf143e7a3b3da1
Paul Blew <pblew@breadnats.co.uk>
2015-07-28 18:50:52 +0100

    PUB#1 - Better drag-and-drop edge selection

--- static/js/publisher/SelectModel.js
+++ static/js/publisher/SelectModel.js
@@ -4,11 +4,16 @@ define(["knockout", "knockout-dragdrop", "dcl/dcl"], function (ko, kos, declare)
         var incident = {};
         ko.utils.objectForEach(response||{}, function (name, value) {
             if (name === "edges") {
-                incident.edges = ko.utils.arrayFilter(value, function (edge) {
+                ko.utils.arrayForEach(value, function (edge) {
                     var type = edge["ty"];
-                    return type === "ttp" || type === "ind"
-                        || type === "coa" || type === "inc";
-                })
+                    if (type === "ttp" || type === "ind" || type === "coa" || type === "inc") {
+                        edge._selectable = ko.observable(true);
+                    } else {
+                        edge._selectable = ko.observable(false);
+                    }
+                    edge._visible = ko.observable(true);
+                });
+                incident.edges = value;
             } else if (name === "success" || name === "error_message") {
                 // ignore these properties
             } else {
@@ -61,6 +66,7 @@ define(["knockout", "knockout-dragdrop", "dcl/dcl"], function (ko, kos, declare)
         },
 
         _onSelectionChanged: function (newId) {
+            this.availableEdges.removeAll();
             this.selectedEdges.removeAll();
             if (newId) {
                 postJSON("/catalog/ajax/get_object/", {
@@ -82,13 +88,13 @@ define(["knockout", "knockout-dragdrop", "dcl/dcl"], function (ko, kos, declare)
         },
 
         onSelected: function (data, model) {
-            this.availableEdges.remove(data);
+            data._visible(false);
             this.selectedEdges.push(data);
         },
 
         onUnselected: function (data, model) {
             this.selectedEdges.remove(data);
-            this.availableEdges.push(data);
+            data._visible(true);
         }
     });
 });
