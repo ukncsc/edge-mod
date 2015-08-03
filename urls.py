8172ad17c1827898ad03b0c70939320670f6cf89
Paul Silcock <psilcock@breadnats.co.uk>
2015-08-03 15:03:51 +0100

    PUB#1: * Restored changes to URL mappings that were used to test publishing.

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^select/$', 'adapters.publisher.views.select', name='publisher_select'),
                        #    url(r'^review/$', 'adapters.publisher.views.review', name='publisher_review'),
-                       url(r'^ajax/publish/$', 'adapters.publisher.views.ajax_publish', name='publisher_publish'),
+                       #    url(r'^publish/$', 'adapters.publisher.views.publish', name='publisher_publish'),
                        )
 
 navitems = [
