0a7315225aaa851d446767584a751f9e87342556
Paul Blew <pblew@breadnats.co.uk>
2015-08-20 12:06:00 +0100

    PUB#1 - Added highlighting of empty mandatory fields

--- static/js/stix/StixObject.js
+++ static/js/stix/StixObject.js
@@ -2,6 +2,7 @@ define([
     "dcl/dcl",
     "knockout",
     "kotemplate!labelled-field:./templates/labelled-field.html",
+    "kotemplate!mandatory-labelled-field:./templates/mandatory-labelled-field.html",
     "kotemplate!optional-labelled-field:./templates/optional-labelled-field.html",
     "kotemplate!common-fields:./templates/common.html"
 ], function (declare, ko) {
