dc663df5eb1e90127d392915e8c8c9047d5c8005
Paul Blew <pblew@breadnats.co.uk>
2015-09-01 18:36:45 +0100

    #PUB1 - Build process (WIP)

--- static/js/kotemplate.js
+++ static/js/kotemplate.js
@@ -1,6 +1,8 @@
 define(["text"], function (text) {
     "use strict";
 
+    var buildMap = {};
+
     function parse(name) {
         var parts = name.split(":", 2);
         return {
@@ -14,23 +16,41 @@ define(["text"], function (text) {
     }
 
     return {
-        version: "0.0.1",
+        version: "1.0.0",
+
+        normalize: function (name, normalize) {
+            var params = parse(name);
+            return params.id + ":" + normalize(params.name);
+        },
 
         load: function (name, parentRequire, onLoad, config) {
             var params = parse(name);
             text.load(params.name, parentRequire, function (templateText) {
-                if (isBrowser()) {
+                if (config.isBuild) {
+                    buildMap[params.id] = templateText;
+                } else if (isBrowser()) {
                     var scriptTag = document.createElement("script");
                     scriptTag.setAttribute("type", "text/html");
                     scriptTag.setAttribute("id", params.id);
                     scriptTag.appendChild(document.createTextNode(templateText));
                     document.body.appendChild(scriptTag);
                 }
-                onLoad({
-                    id: params.id,
-                    template: templateText
-                });
+                onLoad();
             }, config);
+        },
+
+        write: function (pluginName, moduleName, write) {
+            var params = parse(moduleName);
+            write.asModule(
+                pluginName + "!" + moduleName,
+                    "define(function () {" +
+                        "var scriptTag = document.createElement('script');" +
+                        "scriptTag.setAttribute('type', 'text/html');" +
+                        "scriptTag.setAttribute('id', '" + params.id + "');" +
+                        "scriptTag.appendChild(document.createTextNode('" + text.jsEscape(buildMap[params.id]) + "'));" +
+                        "document.body.appendChild(scriptTag);" +
+                    "});"
+            );
         }
     };
 });
