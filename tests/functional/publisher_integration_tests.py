4f8c06711be9198731e1c1f9070f73d962b09637
Paul Silcock <psilcock@breadnats.co.uk>
2015-09-08 10:26:01 +0100

    * Removed unused variable.

--- tests/functional/publisher_integration_tests.py
+++ tests/functional/publisher_integration_tests.py
@@ -2,7 +2,6 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'repository.settings'
 
-import mongoengine
 import subprocess
 import repository.test as edge_test
 from publisher_edge_object import PublisherEdgeObject
@@ -13,7 +12,6 @@ class PublisherIntegrationTests(edge_test.TestCase):
     fixtures = ['stix']
 
     def loaddata(self, labels):
-        db = mongoengine.connection.get_db()
         for label in labels:
             pathname = os.path.dirname(__file__) + '/fixtures/' + label + '.json'
             subprocess.Popen(['mongoimport', '-d', edge_test.TEST_DB_NAME, '-c', label, '--file', pathname],
