738d3073d5ca7cab3223118ae008ba9877082267
Paul Silcock <psilcock@breadnats.co.uk>
2015-10-20 15:27:12 +0100

    #3: Adapter 'self-registers' any core view replacements.

--- urls.py
+++ urls.py
@@ -1,13 +1,29 @@
 
-from django.conf.urls import patterns, url
+from url_helper import replace_patterns
+from django.conf.urls import patterns, url, include
+from indicator.urls import urlpatterns as original_indicator_urls
+from repository.urls import urlpatterns as core_url_patterns
 
-urlpatterns = patterns(
-    'adapters.cert-ind-build',
-    url(r'^static/(?P<path>[\S]+)$', 'views.static', name='cert_ind_build_static_content'),
-    url(r'^build/$', 'views.indicator_build', name='cert_ind_build')
+
+urlpatterns = []
+
+url_overrides = replace_patterns(
+    original_indicator_urls,
+    '',
+    patterns(
+        'adapters.cert-ind-build',
+        url(r'^static/(?P<path>[\S]+)$', 'views.static', name='cert_ind_build_static_content'),
+        url(r'^build/$', 'views.indicator_build', name='cert_ind_build')
+    )
 )
 
-navitems = [
-    ('CERT-UK Indicator Builder', 'cert_ind_build')
-]
+core_url_patterns = replace_patterns(
+    core_url_patterns,
+    '',
+    patterns(
+        '',
+        url(r'^indicator/', url_overrides)
+    )
+)
 
+navitems = []
