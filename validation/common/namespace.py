cf935419ab4b7733604c4bbc1434c6fcaad6b252
Paul Blew <pblew@breadnats.co.uk>
2015-11-19 15:11:33 +0000

    MOD#2: If an object's namespace is different to the local namespace, no validation is done and a single warning is generated

---
+++ validation/common/namespace.py
@@ -0,0 +1,28 @@
+from clippy.models import CLIPPY_TYPES
+from edge import LOCAL_ALIAS
+from adapters.certuk_mod.validation import FieldValidationInfo, ValidationStatus, ObjectValidationInfo
+
+
+class NamespaceValidationInfo(ObjectValidationInfo):
+
+    def __init__(self, **field_validation):
+        super(NamespaceValidationInfo, self).__init__(**field_validation)
+        self.namespace = field_validation.get(r'namespace')
+
+    @classmethod
+    def validate(cls, type_, id_):
+        field_validation = {}
+        if not NamespaceValidationInfo.__id_is_current_ns(id_):
+            field_validation[r'namespace'] = FieldValidationInfo(
+                ValidationStatus.WARN,
+                r'%s originates from an external namespace' % (CLIPPY_TYPES[type_] or r'Object')
+            )
+        return cls(**field_validation)
+
+    @staticmethod
+    def __id_is_current_ns(id_):
+        ns_prefix = LOCAL_ALIAS + r':'
+        return str(id_).startswith(ns_prefix)
+
+    def is_local(self):
+        return self.namespace is None
--- validation/package/validator.py
+++ validation/package/validator.py
@@ -1,7 +1,9 @@
 
 from edge.combine import STIXPackage
+from edge.tools import nested_get
 import json
 from structure import ObservableStructureConverter, IndicatorStructureConverter
+from adapters.certuk_mod.validation.common.namespace import NamespaceValidationInfo
 from adapters.certuk_mod.validation.observable.validator import ObservableValidator
 from adapters.certuk_mod.validation.indicator.validator import IndicatorValidator
 from adapters.certuk_mod.validation import FieldAlias
@@ -18,23 +20,31 @@ class PackageValidationInfo(object):
         validation = {}
         validation.update(PackageValidationInfo.__validate_observables(package_dict))
         validation.update(PackageValidationInfo.__validate_indicators(package_dict))
-        # and other types...
+        # and other types - namespace only checks...
+        validation.update(PackageValidationInfo.__validate_other(package_dict, r'coa', [r'courses_of_action']))
+        validation.update(PackageValidationInfo.__validate_other(package_dict, r'ttp', [r'ttps',r'ttps']))
+        validation.update(PackageValidationInfo.__validate_other(package_dict, r'inc', [r'incidents']))
         return validation
 
     @staticmethod
     def __validate_observables(package_dict):
-        observables = package_dict.get('observables', {}).get('observables', {})
+        observables = nested_get(package_dict, [r'observables', r'observables'])
         observable_validation = {}
         for observable in observables:
-            if 'observable_composition' not in observable:
-                properties = observable['object']['properties']
-                properties = ObservableStructureConverter.package_to_simple(properties.get('xsi:type'), properties)
-                id_ = observable['id']
-                validation_results = ObservableValidator.validate(
-                    object_type=FieldAlias('xsi:type', properties.get('xsi:type')),
-                    description=observable.get('description'), **properties)
-                if validation_results and validation_results.validation_dict:
-                    observable_validation.update({id_: validation_results.validation_dict})
+            id_ = observable['id']
+            namespace_validation = NamespaceValidationInfo.validate(r'obs', id_)
+            if namespace_validation.is_local():
+                if 'observable_composition' not in observable:
+                    properties = observable['object']['properties']
+                    properties = ObservableStructureConverter.package_to_simple(properties.get('xsi:type'), properties)
+                    validation_results = ObservableValidator.validate(
+                        object_type=FieldAlias('xsi:type', properties.get('xsi:type')),
+                        description=observable.get('description'), **properties)
+                    if validation_results and validation_results.validation_dict:
+                        observable_validation.update({id_: validation_results.validation_dict})
+            else:
+                observable_validation.update({id_: namespace_validation.validation_dict})
+
         return observable_validation
 
     @staticmethod
@@ -43,11 +53,28 @@ class PackageValidationInfo(object):
         indicator_validation = {}
         for indicator in indicators:
             id_ = indicator['id']
-            indicator_properties = IndicatorStructureConverter.package_to_simple(indicator)
-            validation_results = IndicatorValidator.validate(**indicator_properties)
-            if validation_results and validation_results.validation_dict:
-                indicator_validation.update({id_: validation_results.validation_dict})
+            namespace_validation = NamespaceValidationInfo.validate(r'ind', id_)
+            if namespace_validation.is_local():
+                indicator_properties = IndicatorStructureConverter.package_to_simple(indicator)
+                validation_results = IndicatorValidator.validate(**indicator_properties)
+                if validation_results and validation_results.validation_dict:
+                    indicator_validation.update({id_: validation_results.validation_dict})
+            else:
+                indicator_validation.update({id_: namespace_validation.validation_dict})
         return indicator_validation
+
+    @staticmethod
+    def __validate_other(package_dict, type_, path):
+        other_objects = nested_get(package_dict, path)
+        other_validation = {}
+        for other_object in other_objects:
+            id_ = other_object['id']
+            namespace_validation = NamespaceValidationInfo.validate(type_, id_)
+            if namespace_validation.is_local():
+                pass
+            else:
+                other_validation.update({id_: namespace_validation.validation_dict})
+        return other_validation
 
     def to_json(self):
         return json.dumps(self.validation_dict)
