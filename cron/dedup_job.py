9877b0dbb147096aef7d1619ad97fbfd71da1daf
Paul Blew <pblew@breadnats.co.uk>
2015-11-30 17:27:43 +0000

    MOD#4: Only objects from the local namespace are considered for de-duplication

--- cron/dedup_job.py
+++ cron/dedup_job.py
@@ -9,11 +9,13 @@ if not hasattr(settings, 'BASE_DIR'):
     raise Exception("Failed to import Django settings")
 from celery import Celery
 app = Celery('caches', config_source='repository.celeryconfig')
+from edge import LOCAL_ALIAS
 from edge.tools import FileLockOrFail, CannotLock
 from crashlog import models as crashlog
 from mongoengine.connection import get_db
 
 
+LOCAL_ALIAS_REGEX = '^%s:' % LOCAL_ALIAS
 LOCKNAME = os.path.join(settings.LOCK_DIR, 'dedup_job.lock')
 
 
@@ -21,6 +23,9 @@ def find_duplicates(db, type_):
     return db.stix.aggregate([
         {
             '$match': {
+                '_id': {
+                    '$regex': LOCAL_ALIAS_REGEX
+                },
                 'type': type_
             }
         },
@@ -48,7 +53,7 @@ def find_duplicates(db, type_):
 
 def deduplicate(master, dups):
     print "%s: %s" % (master, dups)
-    # TODO: the actual dedeplication - can we use edge/remap or edge/inbox::user_reuser ?
+    # TODO: the actual de-duplication - can we use edge/remap or edge/inbox::user_reuser ?
 
 
 @app.task(ignore_result=True, queue='mapreduce')
