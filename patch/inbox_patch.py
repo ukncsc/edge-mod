66e360931afea0e72e943dbd0a2d4f53c34697d2
Paul Silcock <psilcock@breadnats.co.uk>
2016-01-22 15:49:56 +0000

    MOD#3: Added example post-inbox-ingest method to set 'created_on' field to that of the STIX timestamp, and to set accordingly for child Observables.

--- patch/inbox_patch.py
+++ patch/inbox_patch.py
@@ -3,6 +3,7 @@ import inspect
 from adapters.certuk_mod.builder.kill_chain_definition import KILL_CHAIN_ID
 from edge.inbox import InboxProcessorForBuilders
 from stix.common.kill_chains import KillChainPhaseReference, KillChainPhasesReference
+from signalregistry import SignalRegistry
 
 
 def get_previous_frame():
@@ -36,5 +37,94 @@ def add(self, inbox_item):
     super(InboxProcessorForBuilders, self).add(inbox_item)
 
 
+def update_created_on(content, user, txn_id, **kwargs):
+    from dateutil.parser import parse as date_time_parse
+    from mongoengine.connection import get_db
+
+    query = {
+        '_id': {
+            '$in': content
+        }
+    }
+    projection = {
+        'data.edges': 1,
+        'data.api.timestamp': 1,
+        'data.summary.type': 1,
+        'type': 1,
+        'created_on': 1
+    }
+    objects = {doc['_id']: doc for doc in get_db().stix.find(query, projection)}
+
+    top_level_objects = {_id: objects[_id] for _id in objects if objects[_id]['type'] != 'obs'}
+
+    # Update non-observables, which should always have timestamp:
+    for _id in top_level_objects:
+        doc = top_level_objects[_id]
+        if doc['type'] != 'obs':
+            actual_date = doc['data']['api'].get('timestamp', {})
+            if actual_date:
+                # Update the created_on date:
+                actual_date = date_time_parse(actual_date)
+                # The saved 'created_on' dates are UTC, but technically aren't offset-aware...
+                # However, parsing a string returns an offset-aware datetime object, which we won't be able to compare
+                #  with offset-naive datetimes....
+                # So let's derive the offset from UTC, subtract it from the hour, then remove the timezone info...
+                # ffs
+                actual_date = (actual_date - actual_date.tzinfo.utcoffset(actual_date)).replace(tzinfo=None)
+                get_db().stix.update({
+                    '_id': _id
+                }, {
+                    '$set': {
+                        'created_on': actual_date
+                    }
+                })
+                doc['created_on'] = actual_date
+
+    observables = {
+        _id: objects[_id] for _id in objects if objects[_id]['type'] == 'obs'
+    }
+
+    # Inspect the children of all top-level objects... for those that are observables (and that are in scope here),
+    #  update their 'created_on' dates to that of the top-level object...
+    for _id in top_level_objects:
+        top_level = top_level_objects[_id]
+        if top_level['type'] != 'pkg':
+            for edge_id in top_level['data']['edges']:
+                if edge_id in observables:
+                    current_obs = observables[edge_id]
+                    # Observable compositions can have child observables/observable compositions, so we need to explore
+                    # the entire tree...
+                    queue = [current_obs]
+                    all_obs_children = [edge_id] if top_level['created_on'] < current_obs['created_on'] else []
+                    while len(queue):
+                        obs = queue.pop()
+                        obs_edge_ids = [edge_id for edge_id in obs.get('data', {}).get('edges', {}) if
+                                        edge_id in observables and edge_id not in all_obs_children and top_level[
+                                            'created_on'] < observables[edge_id]['created_on']]
+                        # This guards against duplicate references, but should never happen...
+                        obs_edge_ids = list(set(obs_edge_ids))
+
+                        all_obs_children.extend(obs_edge_ids)
+
+                        queue.extend([observables[_id] for _id in obs_edge_ids])
+
+                    # Finally update the observables' date:
+                    if all_obs_children:
+                        get_db().stix.update({
+                            '_id': {
+                                '$in': all_obs_children
+                            }
+                        }, {
+                            '$set': {
+                                'created_on': top_level['created_on']
+                            }
+                        }, multi=True)
+                    # ...and locally...
+                    for obs_id, obs in observables.iteritems():
+                        if obs_id in all_obs_children:
+                            obs['created_on'] = top_level['created_on']
+
+
 def apply_patch():
+    SignalRegistry().signal('inbox.post-merge').connect(update_created_on)
     InboxProcessorForBuilders.add = add
