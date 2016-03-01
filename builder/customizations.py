1ed7a88a99c8cc97bfbf3a152cc8380cb67d4553
agibson <agibson@breadnats.co.uk>
2016-03-01 11:30:01 +0000

    MOD#20 Fix to loading of potential coas

--- builder/customizations.py
+++ builder/customizations.py
@@ -1,4 +1,4 @@
-from adapters.certuk_mod.patch import indicator_patch, inbox_patch, observable_patch, hash_patch
+from adapters.certuk_mod.patch import indicator_patch, inbox_patch, observable_patch, hash_patch, remap_patch
 from adapters.certuk_mod.builder import CUSTOM_OBSERVABLES
 from adapters.certuk_mod.builder.cert_observable_object_generator import CERTObservableObjectGenerator
 from adapters.certuk_mod.builder.view_seed_data import CERTIndicatorBuilderTemplateDataGenerator
@@ -9,6 +9,7 @@ from indicator import views as original_views
 def apply_customizations():
     indicator_patch.apply_patch()
     inbox_patch.apply_patch()
+    remap_patch.apply_patch()
     observable_patch.apply_patch(CUSTOM_OBSERVABLES)
     hash_patch.apply_patch()
     original_views.observable_object_generator = CERTObservableObjectGenerator()
