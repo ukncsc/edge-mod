b2b3f8dfe44819335b03c78a2b88a9d0035e026d
Paul Blew <paul.blew@gmail.com>
2016-03-04 18:26:05 +0000

    MOD#8 - AMD/DCL version of visualisation PoC (WIP)

--- static/js/visualiser/graph/Graph.js
+++ static/js/visualiser/graph/Graph.js
@@ -7,26 +7,25 @@ define([
 ], function (declare, ko, d3, Link, Node) {
     "use strict";
 
+    function isGetterSetter(value) {
+        return typeof value === "function" && value.length === 1;
+    }
+
     return declare(null, {
         declaredClass: "Graph",
         constructor: function (graphData) {
-            this.links = ko.observableArray(graphData.links);
-            this.nodes = ko.observableArray(graphData.nodes);
-
             var _d3Layout = d3.layout.force();
             this.d3Layout = ko.computed(function () {
                 return _d3Layout;
             });
-console.log("D3:", _d3Layout);
+            // create observables for all [gs]etters on _d3Layout with the default value provided by D3
             ko.utils.objectForEach(_d3Layout, function (name, value) {
-                if (name === "links" || name === "nodes") {
-                    return;
-                }
-                if (typeof value === "function" && value.length === 1) {
+                if (isGetterSetter(value)) {
                     this[name] = ko.observable(value());
                 }
             }.bind(this));
-console.log(this);
+            this.links = ko.observableArray(graphData.links);
+            this.nodes = ko.observableArray(graphData.nodes);
         },
         applyBindingValues: function (bindingValues) {
             ko.utils.objectForEach(bindingValues, function (name, value) {
@@ -34,6 +33,24 @@ console.log(this);
                     this[name](value);
                 }
             }.bind(this));
+            // set up so that changes in our observables are propagated through to D3
+            var _d3Layout = this.d3Layout();
+            var _pendingUpdate = null;
+            ko.utils.objectForEach(this, function (name, value) {
+                if (ko.isObservable(value)) {
+                    value.subscribe(function (newValue) {
+                        _d3Layout[name](newValue);
+                        if (_pendingUpdate) {
+                            clearTimeout(_pendingUpdate);
+                        }
+                        _pendingUpdate = setTimeout(_d3Layout.start.bind(_d3Layout), 50);
+                    });
+                }
+            }.bind(this));
+        },
+        runModel: function () {
+            this.d3Layout().start();
+
         },
         appendData: function (graphData) {
             //TODO - will need to sort out indexes, etc
--- static/js/visualiser/graph/forceGraphBinding.js
+++ static/js/visualiser/graph/forceGraphBinding.js
@@ -1,11 +1,12 @@
 define([
     "knockout",
     "d3",
-    "jquery"
+    "jquery",
+    "./Graph"
 ], function (ko, d3, $) {
     "use strict";
 
-    function sizeToParent(element) {
+    function sizeToParent(element, graphModel) {
         var parent = $(element.parentNode);
         var height = parent.height();
         var width = parent.width();
@@ -13,9 +14,12 @@ define([
             .attr("viewBox", "0 0 " + width + " " + height)
             .attr("height", height)
             .attr("width", width);
+        graphModel.size([width, height]);
+        graphModel.runModel();
     }
 
     ko.bindingHandlers.forceGraph = {
+        nodeClass: "ko-d3-graph-node",
         init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
             // This will be called when the binding is first applied to an element
             // Set up any initial state, event handlers, etc. here
@@ -25,13 +29,17 @@ define([
             if (!(element.firstElementChild && element.firstElementChild.tagName === "g")) {
                 throw new Error("The node template must be a 'g' element.");
             }
-            d3.select(window).on("resize", function () {
-                sizeToParent(element);
-            });
-            sizeToParent(element);
+            element.firstElementChild.setAttribute("class", ko.bindingHandlers.forceGraph.nodeClass);
 
             var graphModel = valueAccessor()();
+            var nodeContext = bindingContext.createChildContext(graphModel.nodes);
+            ko.bindingHandlers.foreach.init(element, graphModel.nodes, allBindings, viewModel, nodeContext);
             graphModel.applyBindingValues(allBindings());
+
+            d3.select(window).on("resize", function () {
+                sizeToParent(element, graphModel);
+            });
+            sizeToParent(element, graphModel);
 
             // Tell Knockout that we've already dealt with child bindings
             return {
@@ -42,6 +50,20 @@ define([
             // This will be called once when the binding is first applied to an element,
             // and again whenever any observables/computeds that are accessed change
             // Update the DOM element based on the supplied values here.
+            var graphModel = valueAccessor()();
+            var nodeContext = bindingContext.createChildContext(graphModel.nodes);
+            ko.bindingHandlers.foreach.update(element, graphModel.nodes, allBindings, viewModel, nodeContext);
+
+            var container = d3.select(element);
+            var nodeSelector = container
+                .selectAll("g." + ko.bindingHandlers.forceGraph.nodeClass)
+                .data(graphModel.nodes());
+
+            graphModel.d3Layout().on("tick", function () {
+                nodeSelector.attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                })
+            });
         }
     };
 });
