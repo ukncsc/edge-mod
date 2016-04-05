78ab52eb87c0fd3074638812e149ed19c14341a3
Paul Blew <pblew@breadnats.co.uk>
2016-04-05 12:40:16 +0100

    MOD#8 - Extracted graph data loading from constructor so that it can also be called later

--- static/js/visualiser/graph/Graph.js
+++ static/js/visualiser/graph/Graph.js
@@ -33,13 +33,6 @@ define([
                     this[name] = proxy;
                 }
             }.bind(this));
-            this.nodes(ko.utils.arrayMap(graphData.nodes, function (nodeData) {
-                return new Node(nodeData);
-            }));
-            var /*Node[]*/ _rawNodes = this.nodes.peek();
-            this.links(ko.utils.arrayMap(graphData.links, function (linkData) {
-                return new Link(_rawNodes[linkData.source], _rawNodes[linkData.target]);
-            }.bind(this)));
             this.selectedNode = ko.observable(null);
             this.selectedLinkedNodes = ko.computed(function () {
                 var selectedNode = this.selectedNode();
@@ -65,6 +58,16 @@ define([
                 }
                 return linkedNodes;
             }, this).extend({rateLimit: 50});
+            this.loadData(graphData);
+        },
+        loadData: function (graphData) {
+            this.nodes(ko.utils.arrayMap(graphData.nodes, function (nodeData) {
+                return new Node(nodeData);
+            }));
+            var /*Node[]*/ _rawNodes = this.nodes.peek();
+            this.links(ko.utils.arrayMap(graphData.links, function (linkData) {
+                return new Link(_rawNodes[linkData.source], _rawNodes[linkData.target]);
+            }.bind(this)));
         },
         applyBindingValues: function (bindingValues) {
             ko.utils.objectForEach(bindingValues, function (name, value) {
