c9a5cea5d763076465933223235f83c9115b2f1a
Paul Blew <pblew@breadnats.co.uk>
2016-05-09 10:28:15 +0100

    PROTO: Added geographic and Hilbert map visualisations

--- static/js/dashboard/DashboardModel.js
+++ static/js/dashboard/DashboardModel.js
@@ -11,14 +11,14 @@ define([
                 {label:"Indicators by Type",imageUrl:"chart1.png"},
                 {label:"Incidents by Sector",imageUrl:"chart2.png"},
                 {label:"Total Objects",imageUrl:"chart3.png"},
-                {label:"Duplication",imageUrl:"chart4.png"},
+                {label:"Geographic Spread", imageUrl:"geo2.png"},
+                {label:"Hilbert Map", imageUrl:"hilbert.png"},
                 {label:"Object Count by Source",imageUrl:"chart5.png"},
                 {label:"Quality by Source",imageUrl:"chart6.png"},
                 {label:"Response Times",imageUrl:"chart7.png"},
                 {label:"Observable Types by Source",imageUrl:"chart8.png"},
-                {label:"Geographic Spread",imageUrl:"chart9.png"},
-                {label:"Incidents by Actor",imageUrl:"chart1.png"},
-                {label:"Incidents by TTP",imageUrl:"chart2.png"}
+                {label:"Incidents by TTP",imageUrl:"chart2.png"},
+                {label:"Duplication", imageUrl:"chart4.png"}
             ]);
         }
     });
