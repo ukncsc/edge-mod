8179ac06b20f250e3acc26437bbd15936ed11634
matt.carr <matt.carr@purplesecure.com>
2016-05-31 10:32:23 +0100

    Mock namespace config

--- tests/functional/test_dedup_validation_integration.py
+++ tests/functional/test_dedup_validation_integration.py
@@ -1,6 +1,7 @@
 import os
 import io
 import subprocess
+import mock
 
 import repository.test as edge_test
 from adapters.certuk_mod.dedup.DedupInboxProcessor import DedupInboxProcessor
@@ -29,7 +30,7 @@ class DedupFunctionalTests(edge_test.TestCase):
         self.assertRaises(InboxError, ip.run)
 
     def loaddata(self, labels):
-         for label in labels:
+        for label in labels:
             pathname = os.path.dirname(__file__) + '/ttps/' + label + '.json'
             subprocess.Popen(['mongoimport', '-d', edge_test.TEST_DB_NAME, '-c', label, '--file', pathname],
                              stdout=subprocess.PIPE).communicate()
@@ -75,18 +76,23 @@ class DedupFunctionalTests(edge_test.TestCase):
         merge = 'Merged 3 local namespace TTPs in the supplied package based on CAPEC-IDs and title'
         self.assertEqual(ip.filter_messages[1], merge)
 
+    @mock.patch('adapters.certuk_mod.dedup.DedupInboxProcessor.LOCAL_NAMESPACE', 'http://www.purplesecure.com')
     def test_DedupInboxProcessor_validate_correct_tgt_dedup_message_merge(self):
         ip = self.create_inbox_from_file('TGT-PackageWithMerge.xml')
         ip.run()
         merge = 'Merged 2 local namespace Exploit Targets in the supplied package based on CVE-IDs'
         self.assertEqual(ip.filter_messages[1], merge)
 
+    @mock.patch('adapters.certuk_mod.dedup.DedupInboxProcessor.LOCAL_NAMESPACE', 'http://www.purplesecure.com')
+    @mock.patch('adapters.certuk_mod.dedup.property_finder.LOCAL_NAMESPACE', 'http://www.purplesecure.com')
     def test_DedupInboxProcessor_validate_correct_tgt_dedup_message_remapping(self):
         ip = self.create_inbox_from_file('TGT-PackageWithRemapping.xml')
         ip.run()
         remap = 'Remapped 2 local namespace Exploit Targets to existing Targets based on CVE-IDs'
         self.assertEqual(ip.filter_messages[1], remap)
 
+    @mock.patch('adapters.certuk_mod.dedup.DedupInboxProcessor.LOCAL_NAMESPACE', 'http://www.purplesecure.com')
+    @mock.patch('adapters.certuk_mod.dedup.property_finder.LOCAL_NAMESPACE', 'http://www.purplesecure.com')
     def test_DedupInboxProcessor_validate_correct_tgt_dedup_message_merge_and_remapping(self):
         ip = self.create_inbox_from_file('TGT-PackageWithMergeAndRemapping.xml')
         ip.run()
@@ -94,4 +100,3 @@ class DedupFunctionalTests(edge_test.TestCase):
         remap = 'Remapped 2 local namespace Exploit Targets to existing Targets based on CVE-IDs'
         self.assertEqual(ip.filter_messages[1], merge)
         self.assertEqual(ip.filter_messages[2], remap)
-
