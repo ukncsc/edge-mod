c6e612e2d29ee7df6a8f6a9f574fea89e0052eff
andy.gibson <andy.gibson@purplesecure.com>
2016-06-07 11:06:17 +0100

    MOD#164 Separate tooltip code. Too tightly bound currently

--- static/js/visualiser/graph/forceGraphBinding.js
+++ static/js/visualiser/graph/forceGraphBinding.js
@@ -4,8 +4,9 @@ define([
     "jquery",
     "common/topic",
     "./topics",
-    "./Graph"
-], function (ko, d3, $, topic, topics) {
+    "./Graph",
+    "./ToolTip"
+], function (ko, d3, $, topic, topics, Graph, ToolTip) {
     "use strict";
 
     function noPlusButton(hasBacklinks, hasMatches, isBackLinkShown, isMatchesShown) {
@@ -81,7 +82,8 @@ define([
             var maxZoom = 5;
 
             var parent = d3.select(element.parentElement);
-            var tooltip = parent.select('#graph-node-tooltip');
+
+            var tooltip = new ToolTip(parent.select('#graph-node-tooltip'), container, viewModel);
 
             var zoom = d3.behavior.zoom()
                 .scaleExtent([minZoom, maxZoom])
@@ -132,87 +134,17 @@ define([
                 .selectAll("." + ko.bindingHandlers.forceGraph.nodeClass)
                 .data(graphModel.nodes()).call(dragNode);
 
-            function createToggleButton(type, glyphicon, state, callback, id) {
-                return "<button type=\"button\" class=\"btn btn-default clear_bg\" aria-label=\"Left Align\" data-bind=\"click:$data." + callback + ".bind($data,'" + id + "')\">"
-                    + "<span class='" + type + " glyphicon glyphicon-" + glyphicon + (state ? " fa-signal " : " ") + "clear_bg'>"
-                    + "</span>"
-                    + "</button>";
-            }
 
-            function createButton(type, glyphicon, callback, id) {
-                return "<button type=\"button\" class=\"btn btn-default clear_bg\" aria-label=\"Left Align\" data-bind=\"click:$data." + callback + ".bind($data,'" + id + "')\">"
-                    + "<span class='" + type + " glyphicon glyphicon-" + glyphicon + " clear_bg'>"
-                    + "</span>"
-                    + "</button>";
-            }
-
-            function showMatchesButton(id, hasMatches, matchesShown) {
-                return hasMatches ?
-                    createToggleButton("match icon-rotated", "pause", matchesShown, (matchesShown ? "onMinusMatchesClicked" : "onPlusMatchesClicked"), id)
-                    : "";
-            }
-
-            function showBacklinksButton(id, hasBacklinks, backlinksShown) {
-                return hasBacklinks ?
-                    createToggleButton("backlink", "arrow-left", backlinksShown, (backlinksShown ? "onMinusBacklinkClicked" : "onPlusBacklinkClicked"), id)
-                    : "";
-            }
-
-            function showEdgesButton(id, hasEdges, edgesShown) {
-                return hasEdges ?
-                    createToggleButton("backlink", "eye-open", edgesShown, (edgesShown ? "onHideEdges" : "onShowEdges"), id)
-                    : "";
-            }
-
-            function showPublishAndHomeButton(id, nodeType) {
-                if (nodeType == 'draft') {
-                    return "";
-                }
-
-                return createButton("", "home", "onNewRootId", id) + createButton("", "info-sign", "onExternalPublish", id);
-            }
 
             var matchingAndBacklinks = container
                 .selectAll("." + ko.bindingHandlers.forceGraph.nodeClass)
                 .data(graphModel.nodes()).on("mousemove", function (d) {
-                    var viewBox = container.node().viewBox;
-                    var x_middle = (viewBox.animVal != null ? viewBox.animVal.width / 2 : 0);
-                    var y_middle = (viewBox.animVal != null ? viewBox.animVal.height / 2 : 0);
-                    var iWidth = (d.imageWidth() / 4) * currentScale;
-                    var iHeight = (d.imageHeight() / 4) * currentScale;
-                    if (d.nodeType() != 'broken') {
-                        tooltip.transition()
-                            .duration(200)
-                            .style("opacity", 0.8);
-                        tooltip.html("<div class=\"btn-group\" role=\"group\" aria-label=\"\">" +
-                                showBacklinksButton(d.id(), d.hasBacklinks(), d.isBackLinkShown()) +
-                                showMatchesButton(d.id(), d.hasMatches(), d.isMatchesShown()) +
-                                showEdgesButton(d.id(), d.hasEdges(), d.isEdgesShown()) +
-                                showPublishAndHomeButton(d.id(), d.nodeType()) +
-                                "</div>"
-                            )
-                            .style("left", ((x_middle + iWidth + (d.x - x_middle) * currentScale) + currentXOffset) + "px")
-                            .style("top", ((y_middle - iHeight + (d.y - y_middle) * currentScale) + currentYOffset) + "px");
-                    }
-                    else {
-                        tooltip.transition()
-                            .duration(10)
-                            .style("opacity", 0.8);
-                        tooltip.html(
-                            "<button type=\"button\" class=\"btn btn-danger clear_bg\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">" +
-                            "<span class='clear_bg'>Broken node</span>" +
-                            "</button>"
-                            )
-                            .style("left", ((x_middle + iWidth + (d.x - x_middle) * currentScale) + currentXOffset) + "px")
-                            .style("top", ((y_middle - iHeight + (d.y - y_middle) * currentScale) + currentYOffset) + "px");
-                    }
-                    ko.applyBindings(viewModel, tooltip.node().childNodes[0]);
+                    tooltip.setNodeTooltip(d, currentScale, currentXOffset, currentYOffset);
                 });
 
             container.on("mouseover", function (d) {
-                tooltip.transition()
-                    .duration(300)
-                    .style("opacity", 0);
+                tooltip.hide();
+
             });
 
 
