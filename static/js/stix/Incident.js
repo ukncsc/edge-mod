3de0367abb586f01bd32d782c20df5feb43f0c7b
andy.gibson <andy.gibson@purplesecure.com>
2016-06-14 10:31:04 +0100

    MOD#123 Ensure only the root timeline show in external publish view

--- static/js/stix/Incident.js
+++ static/js/stix/Incident.js
@@ -11,7 +11,9 @@ define([
 
     return declare(StixObjectTLP, {
         constructor: function (data, stixPackage) {
-            (new TimeLine()).create_timeline("incidentTimelineSVG", stixPackage._rootId._id, "/adapter/certuk_mod/ajax/incident_timeline/");
+            if (stixPackage._rootId._id == this.id()) {
+                (new TimeLine()).create_timeline("incidentTimelineSVG", stixPackage._rootId._id, "/adapter/certuk_mod/ajax/incident_timeline/");
+            }
             this.status = ko.computed(function () {
                 return stixPackage.safeValueGet(this.id(), this.data(), "status.value");
             }, this);
