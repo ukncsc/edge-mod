dae191f266620ac878469bd3236b2c635cab90ab
joe.walton <joe.walton@purpesecure.com>
2016-06-14 12:35:22 +0100

    MOD#12- readd capsulize patch so it adds all marking structures to stix header

--- builder/customizations.py
+++ builder/customizations.py
@@ -1,4 +1,4 @@
-from adapters.certuk_mod.patch import indicator_patch, observable_patch, incident_patch, clone_views
+from adapters.certuk_mod.patch import indicator_patch, observable_patch, incident_patch, clone_views, capsulize_patch
 from adapters.certuk_mod.builder import CUSTOM_OBSERVABLES
 from adapters.certuk_mod.builder.cert_observable_object_generator import CERTObservableObjectGenerator
 from adapters.certuk_mod.builder.view_seed_data import CERTIndicatorBuilderTemplateDataGenerator
@@ -10,6 +10,7 @@ def apply_customizations():
     # NOTE: only UI customisations should be applied here; core behaviour patches belong in certuk_mod/__init__.py
     indicator_patch.apply_patch()
     incident_patch.apply_patch()
+    capsulize_patch.apply_patch()
     observable_patch.apply_patch(CUSTOM_OBSERVABLES)
     original_indicator_views.observable_object_generator = CERTObservableObjectGenerator()
     original_indicator_views.indicator_builder = IndicatorBuilder(original_indicator_views.observable_object_generator)
