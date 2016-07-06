10e4a7e4b76caa351b716e3bd856853ce7e799b9
matt.carr <matt.carr@purplesecure.com>
2016-07-06 16:28:57 +0100

    Fix tests and add more

--- tests/unit/incident_ingest_tests.py
+++ tests/unit/incident_ingest_tests.py
@@ -8,14 +8,13 @@ from adapters.certuk_mod.ingest.draft_from_rtir import create_time, create_gener
 class IncidentIngestTests(unittest.TestCase):
     def setUp(self):
         self.draft = {
-            'attributed_actors': [],
-            'categories': [''],
+            'categories': '',
             'description': '',
-            'external_ids': [{'source': '', 'id': '14007indicator536.csv'}],
+            'external_ids': [],
             'id': 'pss:inc-00000001-0001-0001-0001-00000001',
             'id_ns': 'http://www.purplesecure.com',
-            'intended_effects': [''],
-            'reporter': {'name': 'Purple', 'specification': {'organisation_info': {'industry_type': ''}}},
+            'intended_effects': '',
+            'reporter': {'identity': {'name': ''}},
             'status': 'Closed',
             'title': 'RTIR 1500',
             'victims': [{'name': '', 'specification': {'organisation_info': {'industry_type': 'Water'}}}],
@@ -55,17 +54,26 @@ class IncidentIngestTests(unittest.TestCase):
                                                    'value': '2014-09-30T00:00:00'}}
         self.assertDictEqual(time, compare_time)
 
-    def test_intended_effects(self):
+    def test_generic_values_without_join(self):
         data = {'CustomField.{Intended Effect}': 'Fraud<br />Brand Damage<br />Advantage - Economic'}
-        intended_effects = create_generic_values(data, 'CustomField.{Intended Effect}', False)
+        generic_values = create_generic_values(data, 'CustomField.{Intended Effect}', False)
         compare_effects = ['Fraud', 'Brand Damage', 'Advantage - Economic']
-        self.assertListEqual(intended_effects, compare_effects)
+        self.assertListEqual(generic_values, compare_effects)
 
-    def test_no_effects(self):
+    def test_generic_values_without_join_no_values(self):
         data = {'CustomField.{Intended Effect}': ''}
-        intended_effects = create_generic_values(data, 'CustomField.{Intended Effect}', False)
-        compare_effects = ['']
-        self.assertListEqual(intended_effects, compare_effects)
+        generic_values = create_generic_values(data, 'CustomField.{Intended Effect}', False)
+        self.assertListEqual(generic_values, [''])
+
+    def test_generic_values_with_join(self):
+        data = {'CustomField.{Reporter Type}': 'Domestic Source<br />Government Agency'}
+        generic_values = create_generic_values(data, 'CustomField.{Reporter Type}', True)
+        self.assertEquals(generic_values, 'Domestic Source, Government Agency')
+
+    def test_generic_values_with_join_no_values(self):
+        data = {'CustomField.{Reporter Type}': ''}
+        generic_values = create_generic_values(data, 'CustomField.{Reporter Type}', True)
+        self.assertEquals(generic_values, '')
 
     def test_no_status(self):
         data = {'Status': ''}
@@ -92,14 +100,67 @@ class IncidentIngestTests(unittest.TestCase):
         self.assertEquals(reporter, compare_reporter)
 
     @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.IDManager')
+    def test_validation_for_draft(self, mock_manager):
+        data = {
+            'id': '1500',
+            'CustomField.{Indicator Data Files}': '14007indicator536.csv',
+            'Created': 'value',
+            'CustomField.{Containment Achieved}': 'value',
+            'CustomField.{First Data Exfiltration}': 'value',
+            'CustomField.{First Malicious Action}': 'value',
+            'CustomField.{Incident Discovery}': 'value',
+            'CustomField.{Incident Reported}': 'value',
+            'CustomField.{Initial Compromise}': 'value',
+            'CustomField.{Restoration Achieved}': 'value',
+            'CustomField.{Description}': 'value',
+            'CustomField.{Category}': 'value',
+            'CustomField.{Reporter Type}': 'value',
+            'CustomField.{Incident Sector}': 'Water',
+            'Status': '',
+            'CustomField.{Intended Effect}': 'value',
+            'Resolved': 'value'
+        }
+        mock_manager().get_new_id.return_value = 'pss:inc-00000001-0001-0001-0001-00000001'
+        draft_validation = initialise_draft(data)[1]
+        validation_result = {'pss:inc-00000001-0001-0001-0001-00000001': {
+            'status': {'status': 'INFO', 'message': 'no value provided for status'}}}
+        self.assertDictEqual(draft_validation, validation_result)
+
+    @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.IDManager')
+    def test_no_validation_for_draft(self, mock_manager):
+        data = {
+            'id': '1500',
+            'CustomField.{Indicator Data Files}': '14007indicator536.csv',
+            'Created': 'value',
+            'CustomField.{Containment Achieved}': 'value',
+            'CustomField.{First Data Exfiltration}': 'value',
+            'CustomField.{First Malicious Action}': 'value',
+            'CustomField.{Incident Discovery}': 'value',
+            'CustomField.{Incident Reported}': 'value',
+            'CustomField.{Initial Compromise}': 'value',
+            'CustomField.{Restoration Achieved}': 'value',
+            'CustomField.{Description}': 'value',
+            'CustomField.{Category}': 'value',
+            'CustomField.{Reporter Type}': 'value',
+            'CustomField.{Incident Sector}': 'Water',
+            'Status': 'value',
+            'CustomField.{Intended Effect}': 'value',
+            'Resolved': 'value'
+        }
+        mock_manager().get_new_id.return_value = 'pss:inc-00000001-0001-0001-0001-00000001'
+        draft_validation = initialise_draft(data)[1]
+        validation_result = {}
+        self.assertDictEqual(draft_validation, validation_result)
+
+    @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.IDManager')
     @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.create_time')
     @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.create_generic_values')
     @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.create_external_ids')
     @mock.patch('adapters.certuk_mod.ingest.draft_from_rtir.status_checker')
-    def test_initialise_draft(self, mock_status, mock_external_ids, mock_generic_valus, mock_time, mock_manager):
+    def test_initialise_draft(self, mock_status, mock_external_ids, mock_generic_values, mock_time, mock_manager):
         mock_status.return_value = 'Closed'
-        mock_external_ids.return_value = ['']
-        mock_generic_valus.return_value = 'Purple'
+        mock_external_ids.return_value = []
+        mock_generic_values.return_value = ''
         mock_time.return_value = {}
         mock_manager().get_namespace.return_value = 'http://www.purplesecure.com'
         mock_manager().get_new_id.return_value = 'pss:inc-00000001-0001-0001-0001-00000001'
