b955b2c31e32c841bb84ecff959fefe278cb3c7f
joe.walton <joe.walton@purpesecure.com>
2016-08-10 09:35:45 +0100

    add revoke and purge to object view

--- static/js/catalog/cert-catalog-purge.js
+++ static/js/catalog/cert-catalog-purge.js
@@ -1,8 +1,9 @@
 define([
     "dcl/dcl",
     "knockout",
+    "common/modal/show-publish-modal",
     "common/modal/show-error-modal"
-], function (declare, ko, showErrorModal) {
+], function (declare, ko, showPublishModal, showErrorModal) {
     "use strict";
 
     return declare(null, {
@@ -11,23 +12,37 @@ define([
             this.label = ko.observable("Purge");
             this.canRevoke = ko.observable("");
             this.canPurge = ko.observable("");
-
+            this.ajaxURI = ko.observable("");
+            this.stixID = ko.observable("");
         },
 
         loadStatic: function (optionsList) {
             this.canRevoke(optionsList.canRevoke);
             this.canPurge(optionsList.canPurge);
+            this.ajaxURI(optionsList.ajax_uri);
+            this.stixID(optionsList.rootId);
+        },
+
+        createParams: function () {
+            return {
+                "id": this.stixID()
+            }
         },
 
         revokeObject: function () {
-           /* postJSON("/adapter/certuk_mod/review/duplicates/" + this.id(),"", function (response) {
-                this.duplicates(response["duplicates"]);
+            postJSON(this.ajaxURI() + "revoke_object/", this.createParams(), function (response) {
+                showPublishModal("Revoked", "The STIX object has been successfully revoked", false);
             }.bind(this), function (error) {
                 showErrorModal(error, false)
-            }.bind(this));     */
+            }.bind(this));
         },
 
         purgeObject: function () {
+            postJSON(this.ajaxURI() + "purge_object/", this.createParams(), function (response) {
+                document.location = '/catalog/' + response['type'] + '/';
+            }.bind(this), function (error) {
+                showErrorModal(error, false)
+            }.bind(this));
 
         }
     });
