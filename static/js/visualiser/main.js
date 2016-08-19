356a0f40c755fa44df39b85bdb8e35ed953d9d3b
andy.gibson <andy.gibson@purplesecure.com>
2016-08-19 15:04:13 +0100

    Publish link now changed

--- static/js/visualiser/main.js
+++ static/js/visualiser/main.js
@@ -9,7 +9,7 @@ require([
         window["rootId"],
         "/adapter/certuk_mod/ajax/visualiser/",
         "/adapter/certuk_mod/ajax/visualiser/item/",
-        "/adapter/certuk_mod/review/",
+        "/object/",
         (new PanelActionsBuilder()).build(),
         function (viewModel) {
             ko.applyBindings(
