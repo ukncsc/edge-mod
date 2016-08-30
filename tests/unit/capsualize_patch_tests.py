01101464151c015617338a2320cf2f17ebe5d85d
andy.gibson <andy.gibson@purplesecure.com>
2016-08-30 09:28:06 +0100

    MOD#243 Some tidying

--- tests/unit/capsualize_patch_tests.py
+++ tests/unit/capsualize_patch_tests.py
@@ -7,6 +7,13 @@ from edge.generic import EdgeError
 
 class CapsualizePatchTests(unittest.TestCase):
 
+    def setUp(self):
+        self.eo = mock.MagicMock()
+        self.eo.ty = 'ind'
+        self.eo_child = mock.MagicMock()
+        self.eo_child.ty = 'obs'
+        self.eo_child.id = "1"
+
     @mock.patch('adapters.certuk_mod.patch.capsulize_patch.EdgeObject')
     def testSimple(self, mock_EdgeObject):
 
@@ -14,14 +21,10 @@ class CapsualizePatchTests(unittest.TestCase):
         mock_obs = mock.MagicMock()
 
         with mock.patch.dict('adapters.certuk_mod.patch.capsulize_patch.PACKAGE_ADD_DISPATCH', {'ind': mock_ind, 'obs': mock_obs}):
-            eo = mock.MagicMock()
-            eo.ty = 'ind'
-            eo_child = mock.MagicMock()
-            eo_child.ty = 'obs'
-            eo_child.id = "1"
-            mock_EdgeObject.load.side_effect = [eo_child]
-            eo.edges = [eo_child]
-            pkg, contents = capsulize_patch(eo, 1, False)
+
+            mock_EdgeObject.load.side_effect = [self.eo_child]
+            self.eo.edges = [self.eo_child]
+            pkg, contents = capsulize_patch(self.eo, 1, False)
             mock_ind.assert_called_once()
             mock_obs.assert_not_called()
             self.assertEqual(len(contents), 1)
@@ -33,14 +36,9 @@ class CapsualizePatchTests(unittest.TestCase):
         mock_obs = mock.MagicMock()
 
         with mock.patch.dict('adapters.certuk_mod.patch.capsulize_patch.PACKAGE_ADD_DISPATCH', {'ind': mock_ind, 'obs': mock_obs}):
-            eo = mock.MagicMock()
-            eo.ty = 'ind'
-            eo_child = mock.MagicMock()
-            eo_child.ty = 'obs'
-            eo_child.id = "1"
-            mock_EdgeObject.load.side_effect = [eo_child]
-            eo.edges = [eo_child]
-            pkg, contents = capsulize_patch(eo, 1, True)
+            mock_EdgeObject.load.side_effect = [self.eo_child]
+            self.eo.edges = [self.eo_child]
+            pkg, contents = capsulize_patch(self.eo, 1, True)
             mock_ind.assert_called_once()
             mock_obs.assert_called_once()
             self.assertEqual(len(contents), 2)
@@ -52,15 +50,11 @@ class CapsualizePatchTests(unittest.TestCase):
         mock_obs = mock.MagicMock()
 
         with mock.patch.dict('adapters.certuk_mod.patch.capsulize_patch.PACKAGE_ADD_DISPATCH', {'ind': mock_ind, 'obs': mock_obs}):
-            eo = mock.MagicMock()
-            eo.ty = 'ind'
-            eo_child = mock.MagicMock()
-            eo_child.ty = 'obs'
-            eo_child.id = "1"
-            eo_child.edges = [eo]
-            mock_EdgeObject.load.side_effect = [eo_child]
-            eo.edges = [eo_child]
-            pkg, contents = capsulize_patch(eo, 1, True)
+
+            self.eo_child.edges = [self.eo]
+            mock_EdgeObject.load.side_effect = [self.eo_child]
+            self.eo.edges = [self.eo_child]
+            pkg, contents = capsulize_patch(self.eo, 1, True)
             mock_ind.assert_called_once()
             mock_obs.assert_called_once()
             self.assertEqual(len(contents), 2)
@@ -72,18 +66,13 @@ class CapsualizePatchTests(unittest.TestCase):
         mock_obs = mock.MagicMock()
 
         with mock.patch.dict('adapters.certuk_mod.patch.capsulize_patch.PACKAGE_ADD_DISPATCH', {'ind': mock_ind, 'obs': mock_obs}):
-            eo = mock.MagicMock()
-            eo.ty = 'ind'
-            eo_child = mock.MagicMock()
-            eo_child.ty = 'obs'
-            eo_child.id = "1"
-            eo_child.edges = [eo]
+            self.eo_child.edges = [self.eo]
 
             def raise_EdgeError(x):
                 raise EdgeError;
 
             mock_EdgeObject.load = raise_EdgeError
-            eo.edges = [eo_child]
-            pkg, contents = capsulize_patch(eo, 1, True)
+            self.eo.edges = [self.eo_child]
+            pkg, contents = capsulize_patch(self.eo, 1, True)
             mock_ind.assert_called_once()
             self.assertEqual(len(contents), 1)
