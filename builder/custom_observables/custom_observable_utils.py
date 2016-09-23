0881bc05486d97e3162cb36da67dfcc64def7235
joe.walton <joe.walton@purplesecure.com>
2016-09-23 14:15:19 +0100

    refactor collapse method

--- builder/custom_observables/custom_observable_utils.py
+++ builder/custom_observables/custom_observable_utils.py
@@ -2,10 +2,4 @@ from edge.tools import rgetattr
 
 
 def collapse_nested_values(value):
-    if isinstance(value, basestring):
-        return value
-    if isinstance(value, int):
-        return str(value)
-    else:
-        return rgetattr(value, ['value'])
-
+    return rgetattr(value, ['value'], value)
