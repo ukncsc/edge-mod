27c543d984ba5ed435996bbb282305c364ce0c4a
andy.gibson <andy.gibson@purplesecure.com>
2016-09-26 14:40:30 +0100

    MOD#392 Fix CRM loop issue

--- static/js/stix/StixPackage.js
+++ static/js/stix/StixPackage.js
@@ -3,10 +3,11 @@ define([
     "knockout",
     "./StixId",
     "./ReviewValue",
+    "./AsyncReviewValue",
     "./ValidationInfo",
     "common/cert-identity",
     "text!config-service"
-], function (declare, ko, StixId, ReviewValue, ValidationInfo, Identity, configService) {
+], function (declare, ko, StixId, ReviewValue, AsyncReviewValue, ValidationInfo, Identity, configService) {
     "use strict";
 
     var crmIsEnabled = false;
@@ -175,7 +176,7 @@ define([
             if (crmIsEnabled !== false) {
                 var identityName = this.retrieveIdentity(object, propertyPath);
                 var validation = this._validationInfo.findByProperty(id, validationPath || propertyPath);
-                return new ReviewValue(identityName, validation.state, validation.message);
+                return new AsyncReviewValue(identityName, validation.state, validation.message);
             } else {
                 return this.safeValueGet(id, object, propertyPath, validationPath);
             }
@@ -198,7 +199,7 @@ define([
 
                 var computedStringRepresentation = this.concatenateIdentities(obsArrayIds);
                 var validation = this._validationInfo.findByProperty(id, validationPath || propertyPath);
-                return new ReviewValue(computedStringRepresentation, validation.state, validation.message);
+                return new AsyncReviewValue(computedStringRepresentation, validation.state, validation.message);
             } else {
                 return this.safeListGet(id, object, propertyPath, valueKey, validationPath, delimiter)
             }
